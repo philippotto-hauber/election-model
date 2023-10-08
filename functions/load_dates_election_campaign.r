842ec380af3b7d570ddcee331c01b2f8ec55d354
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-08 17:54:31 +0200

    add functions

---
+++ functions/load_dates_election_campaign.r
@@ -0,0 +1,13 @@
+load_dates_election_campaign <- function(scenario, year = 2024) {
+
+    if (year == 2024){
+        rawdat <- read.csv(here::here("data", "dataland_polls_2024_scenarios.csv"), stringsAsFactors = FALSE)
+        rawdata <- rawdat[rawdat$scenario == scenario, ]
+    } else {
+        rawdat <- read.csv(here::here("data", "dataland_polls_1984_2023.csv"), stringsAsFactors = FALSE)
+    }
+    start_campaign <- min(as.POSIXct(rawdat$date_published))
+    end_campaign <- load_election_day(year)
+    dates_campaign <- seq(start_campaign, end_campaign, by = "1 day")
+    dates_campaign
+}
