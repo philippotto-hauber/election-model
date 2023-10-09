e481167633bd865f2543f90cff78b9e488497ed1
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-09 20:45:39 +0200

    set parallel chains to 2

--- functions/load_mcmc_options.r
+++ functions/load_mcmc_options.r
@@ -1,7 +1,7 @@
 load_mcmc_options <- function() {
     mcmc_options <- list()
     mcmc_options[["n_chains"]] <- 4
-    mcmc_options[["parallel_chains"]] <- 4
+    mcmc_options[["parallel_chains"]] <- 2
     mcmc_options[["n_warmup"]] <- 100
     mcmc_options[["n_sampling"]] <- 100
     mcmc_options[["n_refresh"]] <- 10
