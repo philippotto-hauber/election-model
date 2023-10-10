6a50492187c8943f80e233f2f3420f9ff22a71d5
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-10 08:00:58 +0200

    use Date instead of POSIX

--- functions/load_dates_election_campaign.r
+++ functions/load_dates_election_campaign.r
@@ -6,7 +6,7 @@ load_dates_election_campaign <- function(scenario, year = 2024) {
     } else {
         rawdat <- read.csv(here::here("data", "dataland_polls_1984_2023.csv"), stringsAsFactors = FALSE)
     }
-    start_campaign <- min(as.POSIXct(rawdat$date_published))
+    start_campaign <- min(as.Date(rawdat$date_published))
     end_campaign <- load_election_day(year = year)
     dates_campaign <- seq(start_campaign, end_campaign, by = "1 day")
     dates_campaign
--- functions/load_election_day.R
+++ functions/load_election_day.R
@@ -1,5 +1,5 @@
 load_election_day <- function(path = here::here("data", "dataland_electoral_calendar.csv"), 
                               year = 2024){
   dat <- read.csv(path)
-  return(as.POSIXct(dat[dat$election_cycle == year, "election_day"]))
+  return(as.Date(dat[dat$election_cycle == year, "election_day"]))
 }
