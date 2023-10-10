aa6a96a62c4fda09404c485e5443e70a7e8a3a35
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-10 22:29:50 +0200

    add prior to plot

--- functions/plot_ppiT.R
+++ functions/plot_ppiT.R
@@ -4,15 +4,15 @@ plot_ppiT <- function(df_draws, df_priors, id_election_day, n_states, plt_title_
     mutate(values = ifelse(values == 0.0, NA, values)) %>%
     ggplot(aes(x = values, color = party, fill = party)) +
     geom_density(alpha = 0.2)+
-    # geom_vline(mapping = aes(xintercept = vals, color = party),
-    #            data = filter(df_ppi, t == election_day, type == !!type),
-    #            linewidth = 1, alpha = 0.4)+
+    geom_vline(mapping = aes(xintercept = value, color = party),
+               data = df_priors,
+               linewidth = 1, alpha = 0.3)+
     ggsci::scale_color_jco() +
     ggsci::scale_fill_jco() +
-    facet_wrap(~territory, nrow = ceiling(sqrt(n_states)), scales = "free")+
+    facet_wrap(~state, nrow = ceiling(sqrt(n_states)), scales = "free")+
     xlab("")+
     labs(title = paste0(plt_title_prefix, ": Posterior densities of the expected vote share on election day"),
-         caption = "Vertical lines indicate the 'true', i.e. simulated values used to generate the polls with which the model is estimated. ")+
+         caption = "Dotted vertical lines: prior mean")+
     theme(legend.position="top",
           plot.caption = ggtext::element_textbox_simple(size = 7, 
                                                         margin = margin(8, 0, 0, 0)
