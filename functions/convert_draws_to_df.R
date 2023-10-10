f0c1dc418d480508f7c5567089cdfed3735b1988
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-10 22:30:03 +0200

    rename territory to state

--- functions/convert_draws_to_df.R
+++ functions/convert_draws_to_df.R
@@ -1,13 +1,13 @@
-convert_draws_to_df <- function(draws, dims_draws, territories, parties){
+convert_draws_to_df <- function(draws, states, parties){
   as.data.frame(draws) %>%
     tibble::rownames_to_column(var = "draw") %>%
     tidyr::pivot_longer(col = -draw, names_to = "tmp", values_to = "values") %>%
-    tidyr::separate(tmp, into = dims_draws) %>% 
+    tidyr::separate(tmp, into = c("t", "party", "state")) %>% 
     mutate(values = ifelse(is.nan(values), NA, values),
            t = as.numeric(t),
            draw = as.numeric(draw),
            party = parties[as.numeric(party)],
-           territory = territories[as.numeric(territory)]) -> df
+           state = states[as.numeric(state)]) -> df
   df
 }
 
