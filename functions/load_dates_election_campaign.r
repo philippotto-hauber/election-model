72861b6adef5553aa9f6806b71b111bfbd2980db
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-11 20:20:46 +0200

    rm arg scenario

--- functions/load_dates_election_campaign.r
+++ functions/load_dates_election_campaign.r
@@ -1,11 +1,10 @@
-load_dates_election_campaign <- function(scenario, year = 2024) {
-
+load_dates_election_campaign <- function(year = 2024) {
     if (year == 2024){
         rawdat <- read.csv(here::here("data", "dataland_polls_2024_scenarios.csv"), stringsAsFactors = FALSE)
-        rawdata <- rawdat[rawdat$scenario == scenario, ]
     } else {
         rawdat <- read.csv(here::here("data", "dataland_polls_1984_2023.csv"), stringsAsFactors = FALSE)
     }
+    # start of campaign = earliest poll -> assume same start in all scenarios
     start_campaign <- min(as.Date(rawdat$date_published))
     end_campaign <- load_election_day(year = year)
     dates_campaign <- seq(start_campaign, end_campaign, by = "1 day")
