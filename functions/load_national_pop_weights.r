e0dfdcb6e60fb92928913c9989f694b8a6a4f726
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-11 19:52:40 +0200

    rm old funcs

--- functions/load_national_pop_weights.r
+++
@@ -1,8 +0,0 @@
-# function to calculate the share of the total population of each state
-# output is a named vector of length equal to the number of states
-load_national_pop_weights <- function(path = here::here("data", "dataland_demographics.csv")){
-    dat <- read.csv(path, stringsAsFactors = FALSE)        
-    pop_weights_nat <- dat$population/sum(dat$population)
-    names(pop_weights_nat) <- dat$province
-    pop_weights_nat
-}
