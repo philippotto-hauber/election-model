c69eff86ba1e6f8e4c444c27831e07776898e782
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-11 07:53:04 +0200

    rename input

--- functions/convert_draws_to_df.R
+++ functions/convert_draws_to_df.R
@@ -1,13 +1,13 @@
-convert_draws_to_df <- function(draws, states, parties){
+convert_draws_to_df <- function(draws, geographies, parties){
   as.data.frame(draws) %>%
     tibble::rownames_to_column(var = "draw") %>%
     tidyr::pivot_longer(col = -draw, names_to = "tmp", values_to = "values") %>%
-    tidyr::separate(tmp, into = c("t", "party", "state")) %>% 
+    tidyr::separate(tmp, into = c("t", "party", "geography")) %>% 
     mutate(values = ifelse(is.nan(values), NA, values),
            t = as.numeric(t),
            draw = as.numeric(draw),
            party = parties[as.numeric(party)],
-           state = states[as.numeric(state)]) -> df
+           geography = geographies[as.numeric(geography)]) -> df
   df
 }
 
