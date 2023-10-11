4de545a5c18da0aeadae76e40c5899fec02b0db3
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-11 07:53:22 +0200

    rename input

--- functions/plot_ppiT.R
+++ functions/plot_ppiT.R
@@ -1,4 +1,4 @@
-plot_ppiT <- function(df_draws, df_priors, id_election_day, n_states, plt_title_prefix) {
+plot_ppiT <- function(df_draws, df_priors, id_election_day, n_geographies, plt_title_prefix) {
   df_draws %>%
     filter(t == id_election_day) %>%
     mutate(values = ifelse(values == 0.0, NA, values)) %>%
@@ -9,12 +9,12 @@ plot_ppiT <- function(df_draws, df_priors, id_election_day, n_states, plt_title_
                linewidth = 1, alpha = 0.3)+
     ggsci::scale_color_jco() +
     ggsci::scale_fill_jco() +
-    facet_wrap(~state, nrow = ceiling(sqrt(n_states)), scales = "free")+
+    facet_wrap(~geography, nrow = ceiling(sqrt(n_geographies)), scales = "free")+
     xlab("")+
     labs(title = paste0(plt_title_prefix, ": Posterior densities of the expected vote share on election day"),
-         caption = "Dotted vertical lines: prior mean")+
+         caption = "Vertical lines: prior mean")+
     theme(legend.position="top",
-          plot.caption = ggtext::element_textbox_simple(size = 7, 
+          plot.caption = ggtext::element_textbox_simple(size = 10, 
                                                         margin = margin(8, 0, 0, 0)
           ),
           axis.text.x = element_text(angle = 0)
