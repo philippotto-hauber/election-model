f9a9d7c5d9a7e4ea9e46d365b0c64ed374bb3c5d
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-11 21:24:06 +0200

    back out original dates

--- functions/convert_draws_to_df.R
+++ functions/convert_draws_to_df.R
@@ -1,10 +1,10 @@
-convert_draws_to_df <- function(draws, geographies, parties){
+convert_draws_to_df <- function(draws, geographies, parties, dates_campaign){
   as.data.frame(draws) %>%
     tibble::rownames_to_column(var = "draw") %>%
     tidyr::pivot_longer(col = -draw, names_to = "tmp", values_to = "values") %>%
     tidyr::separate(tmp, into = c("t", "party", "geography")) %>% 
     mutate(values = ifelse(is.nan(values), NA, values),
-           t = as.numeric(t),
+           t = dates_campaign[as.numeric(t)],
            draw = as.numeric(draw),
            party = parties[as.numeric(party)],
            geography = geographies[as.numeric(geography)]) -> df
