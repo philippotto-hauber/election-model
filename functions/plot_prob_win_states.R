c9be8060516eb83fa6d220307381ec5304c3f98e
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-13 07:36:10 +0200

    add nrows in facet_wrap

--- functions/plot_prob_win_states.R
+++ functions/plot_prob_win_states.R
@@ -1,4 +1,7 @@
 plot_prob_win_states <- function(df_prob_win_states, plt_title_prefix){
+  n_geographies <- length(load_states())
+  stopifnot(n_geographies == length(unique(df_prob_win_states$geography)))
+
   ggplot(df_prob_win_states, 
          aes(x = party, 
              y = prob_win, 
@@ -8,7 +11,7 @@ plot_prob_win_states <- function(df_prob_win_states, plt_title_prefix){
     geom_label(aes(label = paste0(format(round(prob_win * 100, digits = 1), nsmall = 1), " %")), color = "white", size = 2, show.legend = F)+
     ggsci::scale_fill_jco()+
     scale_y_continuous(breaks=seq(0, 1, 0.2), limits = c(0, 1))+
-    facet_wrap(~geography)+
+    facet_wrap(~geography, ceiling(sqrt(n_geographies)))+
     labs(title = paste0(plt_title_prefix, ": Probability of winning electoral votes"))+
     ylab("probability")+
     xlab("") -> plt_probwin_states
