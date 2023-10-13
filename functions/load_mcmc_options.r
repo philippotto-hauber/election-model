404fdf5c041ccaff9078b7e7b48d1326863fceea
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-13 06:20:27 +0200

    adjust mcmc options for estimation

--- functions/load_mcmc_options.r
+++ functions/load_mcmc_options.r
@@ -1,9 +1,9 @@
 load_mcmc_options <- function() {
     mcmc_options <- list()
-    mcmc_options[["n_chains"]] <- 4
+    mcmc_options[["n_chains"]] <- 2    
     mcmc_options[["parallel_chains"]] <- 2
-    mcmc_options[["n_warmup"]] <- 100
-    mcmc_options[["n_sampling"]] <- 100
-    mcmc_options[["n_refresh"]] <- 10
+    mcmc_options[["n_warmup"]] <- 250
+    mcmc_options[["n_sampling"]] <- 1000
+    mcmc_options[["n_refresh"]] <- 100
     mcmc_options
 }
