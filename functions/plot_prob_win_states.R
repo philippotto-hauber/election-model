fca0ab9c986d8038209dfce47b9795b9037a3db0
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-13 19:30:07 +0200

    filter on date

--- functions/plot_prob_win_states.R
+++ functions/plot_prob_win_states.R
@@ -1,8 +1,11 @@
-plot_prob_win_states <- function(df_prob_win_states, plt_title_prefix){
+plot_prob_win_states_over_time <- function(
+    df,
+    filter_t, 
+    plt_title_prefix){
   n_geographies <- length(load_states())
-  stopifnot(n_geographies == length(unique(df_prob_win_states$geography)))
+  stopifnot(n_geographies == length(unique(df$geography)))
 
-  ggplot(df_prob_win_states, 
+  ggplot(filter(df, date == t), 
          aes(x = party, 
              y = prob_win, 
              fill = party)
@@ -12,8 +15,8 @@ plot_prob_win_states <- function(df_prob_win_states, plt_title_prefix){
     ggsci::scale_fill_jco()+
     scale_y_continuous(breaks=seq(0, 1, 0.2), limits = c(0, 1))+
     facet_wrap(~geography, ceiling(sqrt(n_geographies)))+
-    labs(title = paste0(plt_title_prefix, ": Probability of winning electoral votes"))+
+    labs(title = paste0(plt_title_prefix, ": Probability of winning electoral votes on", filter_t))+
     ylab("probability")+
-    xlab("") -> plt_probwin_states
-  plt_probwin_states
+    xlab("") -> plt
+  plt
 }
