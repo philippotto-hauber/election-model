a08ccb0a99310bfa6411d6f9e067cb6b877938d9
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-14 10:09:25 +0200

    add function

---
+++ functions/invert_alr_on_prior.r
@@ -0,0 +1,37 @@
+invert_alr_on_prior <- function(prior_mmu_T) {
+    prior_mmuT <- as.data.frame(priors[[scen]][["m_mmu_T"]])
+    names(prior_mmuT) <- "mmu"   
+    prior_mmuT %>% 
+        tibble::rownames_to_column(var = "tmp") %>%
+        tidyr::separate(
+            tmp, 
+            into = c("state", "party"), 
+            sep = "_") %>% 
+        tidyr::pivot_wider(
+            names_from = "party", 
+            values_from = "mmu") %>% 
+        mutate(SSP = NA) -> df_prior_mmuT
+
+    df_prior_mmuT[is.na(df_prior_mmuT)] <- 0.0
+
+    prior_ppi <- matrix(NA, 
+                        nrow = length(states), 
+                        ncol = length(parties),
+                        dimnames = list(states, parties))
+
+    for (s in seq(1, length(states))) {
+        parties_s <- parties[1:(n_parties_by_state[s])]
+        ppi_s <- inv_additive_log_ratio(unlist(df_prior_mmuT[s, parties_s]))
+        prior_ppi[s, 1:n_parties_by_state[s]] <- ppi_s 
+    }
+
+    prior_ppi %>% 
+        as.data.frame() %>% 
+        tibble::rownames_to_column(var = "geography") %>% 
+        tidyr::pivot_longer(
+            cols = -geography, 
+            names_to = "party", 
+            values_to = "value"
+            ) -> df_prior_ppi
+    df_prior_ppi
+}
