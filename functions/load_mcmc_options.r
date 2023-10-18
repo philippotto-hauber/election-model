11f34566f91d88ca87d8407e823bc586b155d652
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-18 20:57:27 +0200

    lower number of draws

--- functions/load_mcmc_options.r
+++ functions/load_mcmc_options.r
@@ -2,8 +2,8 @@ load_mcmc_options <- function() {
     mcmc_options <- list()
     mcmc_options[["n_chains"]] <- 2    
     mcmc_options[["parallel_chains"]] <- 2
-    mcmc_options[["n_warmup"]] <- 250
-    mcmc_options[["n_sampling"]] <- 1000
+    mcmc_options[["n_warmup"]] <- 100
+    mcmc_options[["n_sampling"]] <- 500
     mcmc_options[["n_refresh"]] <- 100
     mcmc_options
 }
