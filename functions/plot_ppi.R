b2269f77b947473a475c502eae33f2630c4fa358
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-20 13:06:36 +0200

    plt posterior mean instead of median

--- functions/plot_ppi.R
+++ functions/plot_ppi.R
@@ -2,14 +2,14 @@ plot_ppi <- function(df_draws, df_polls, n_geographies, type_of_poll, plt_title_
   df_draws %>%
     mutate(values = ifelse(values == 0.0, NA, values)) %>%
     group_by(t, geography, party) %>%
-    summarise(q_50 = median(values, na.rm = T),
+    summarise(mn = mean(values, na.rm = T),
               q_95_upp = quantile(values, prob = c(0.025), na.rm = T),
               q_95_low = quantile(values, prob = c(1-0.025), na.rm = T),
               q_83_upp = quantile(values, prob = c(0.0855), na.rm = T),
               q_83_low = quantile(values, prob = c(1-0.085), na.rm = T)) %>%
     ungroup() %>%
     ggplot(aes(x = t))+
-    geom_line(aes(y = q_50, color = party), linetype = "dashed", linewidth= 1.0)+
+    geom_line(aes(y = mn, color = party), linetype = "dashed", linewidth= 1.0)+
     geom_ribbon(aes(ymin = q_95_low, ymax = q_95_upp, fill = party), alpha = 0.2)+
     geom_ribbon(aes(ymin = q_83_low, ymax = q_83_upp, fill = party), alpha = 0.3)+
     # add share in polls as points in plot
@@ -20,7 +20,7 @@ plot_ppi <- function(df_draws, df_polls, n_geographies, type_of_poll, plt_title_
     {if (type_of_poll != "national")
       facet_wrap(vars(geography), nrow = ceiling(sqrt(n_geographies)), scales = "free")}+
     labs(title = paste0(plt_title_prefix, ": Expected vote share"),
-         caption = "Posterior median: dashed line; lighter (darker) ribbon: 95 (83) percent posterior credible interval; dots indicate the vote share in a survey on that day")+
+         caption = "Posterior mean: dashed line; lighter (darker) ribbon: 95 (83) percent posterior credible interval; dots indicate the vote share in a survey on that day")+
     ylab("share")+
     theme(legend.position="top",
           plot.caption = ggtext::element_textbox_simple(size = 7, 
