e02b973994bbfc54fc90b771ecfcdf0a0789c101
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-20 23:42:52 +0200

    update batch script

--- run_scripts.bat
+++ run_scripts.bat
@@ -1,7 +1,9 @@
 quarto render ./data/polls_scenarios_2024.qmd
 
+quarto render ./fundamental_forecast/generate_fundamental_forecast.qmd
+
 quarto render ./priors/construct_priors.qmd
 
 Rscript --vanilla ./model/estimate_models.r
 
-
+quarto render ./results/export_and_plot_results.qmd
