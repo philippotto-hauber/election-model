2a70a840e1a727d56a02c38dda1393dd8c9955a4
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-20 06:22:40 +0200

    adjust dates to date_conducted (instead of date_published

--- functions/load_dates_election_campaign.r
+++ functions/load_dates_election_campaign.r
@@ -5,7 +5,7 @@ load_dates_election_campaign <- function(year = 2024) {
         rawdat <- read.csv(here::here("data", "dataland_polls_1984_2023.csv"), stringsAsFactors = FALSE)
     }
     # start of campaign = earliest poll -> assume same start in all scenarios
-    start_campaign <- min(as.Date(rawdat$date_published))
+    start_campaign <- min(as.Date(rawdat$date_conducted))
     end_campaign <- load_election_day(year = year)
     dates_campaign <- seq(start_campaign, end_campaign, by = "1 day")
     dates_campaign
