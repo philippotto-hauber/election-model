76ad2831c4a4a34ba61c1d42e1da20a0e93a1b97
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-26 21:50:24 +0200

    update plot

--- functions/plot_prob_win_election_over_time.r
+++ functions/plot_prob_win_election_over_time.r
@@ -1,14 +1,22 @@
-plot_prob_win_election_over_time <- function(df, plt_title_prefix) {
+plot_prob_win_election_over_time <- function(
+  df,
+  plt_title_prefix,
+  plt_sbtitle = element_blank(),
+  plt_caption = element_blank()
+) {
     df %>%
     ggplot(aes(x = date, y = prob_win, fill = party, color = party)) +
     geom_bar(position = "stack", stat = "identity", width = 1) +
     ggsci::scale_fill_jco() + 
     ggsci::scale_color_jco() +
-    labs(title = paste0(plt_title_prefix, ": Probability of winning the election"))+
+    labs(
+      title = paste0(plt_title_prefix, ": Probability of winning the election"),
+      caption = plt_caption
+    )+
     ylab("probability")+
     xlab("") +
     theme(
-        legend.position = "bottom", 
+        legend.position = "top", 
         legend.title = element_blank()) -> plt
   plt
 }
