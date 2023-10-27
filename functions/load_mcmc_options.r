befb9dcaca02e188a90ce7672e695020297ab90b
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-10-27 22:23:31 +0200

    del files before backtest run

--- functions/load_mcmc_options.r
+++ functions/load_mcmc_options.r
@@ -1,12 +1,12 @@
 load_mcmc_options <- function() {
     mcmc_options <- list()
-    mcmc_options[["n_chains"]] <- 2
-    mcmc_options[["parallel_chains"]] <- 2
+    mcmc_options[["n_chains"]] <- 1
+    mcmc_options[["parallel_chains"]] <- 1
     # minimum number of required warmup iterations is
     # 150 see https://mc-stan.org/docs/2_18/reference-manual/hmc-algorithm-parameters.html#adaptation.figure
-    mcmc_options[["n_warmup"]] <- 150
-    mcmc_options[["n_sampling"]] <- 500
-    mcmc_options[["n_refresh"]] <- 50
-    mcmc_options[["seed"]] <- 15401
+    mcmc_options[["n_warmup"]] <- 10
+    mcmc_options[["n_sampling"]] <- 10
+    mcmc_options[["n_refresh"]] <- 10
+    mcmc_options[["seed"]] <- 27
     mcmc_options
 }
